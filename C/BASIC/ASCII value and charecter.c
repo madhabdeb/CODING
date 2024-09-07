66025548ad25a58d1a5e85db2ca549f3427c09b0
Madhab Deb <125267186+madhabdeb@users.noreply.github.com>
2024-09-08 01:48:28 +0600

    Add files via upload

---
+++ C/BASIC/ASCII value and charecter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    int a;
+        for (a=0;a<=127;a++)
+            printf ("\n\t%d\t%c",a,a);
+
+
+}
+
+
---
+++ C/BASIC/Alignment of a charecter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    char c='A';
+        printf ("%c\n\n",c);
+        printf ("%5c\n\n",c);
+        printf ("%10c\n\n",c);
+        printf ("%5c\n\n",c);
+        printf ("%c\n\n",c);
+
+}
