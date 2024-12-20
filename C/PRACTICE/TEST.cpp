fec47abce3549c8143f53e85da06dd55726af9d5
Madhab Deb <125267186+madhabdeb@users.noreply.github.com>
2024-12-21 01:40:44 +0600

    Add files via upload

---
+++ C/PRACTICE/TEST.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+    int a,b,arr[50],arr2[10][3],n,t;
+    char c,ch[300],ch1[10][100];
+    float f;
+    double d;
+    string ss,sp;
+    vector<string>vs;
+    vector<int>vi;
+    list<int>li;
+  //  vi.resize(3,0);
+
+   for(a=0;a<3;a++)
+   {
+       vi[a]=a+a;
+   }
+     for(a=0;a<3;a++)
+   {
+       cout<<vi[a];
+   }
+
+    return 0;
+}
---
+++ C/PRACTICE/prac.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+
+   char ch[3][100];
+   int a;
+
+   for(a=0;a<3;a++)
+   {
+
+       gets(ch[a]);
+   }
+
+      for(a=0;a<3;a++)
+   {
+
+       puts(ch[a]);
+   }
+
+
+   printf("madhab");
+return 0;
+}
